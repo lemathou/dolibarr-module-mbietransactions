2107f82597a2afcfce6fe04c191204946f7c1f92
Mathieu Moulin <mathieu@iprospective.fr>
2025-09-23 16:07:53 +0200

    Fix tel etransactions up2Pay 3DSecure NOT international

--- payment.php
+++ payment.php
@@ -182,14 +182,16 @@ if (!$confError) {
 	$tel = trim(!empty($thirdparty->phone_mobile) ?$thirdparty->phone_mobile :$thirdparty->phone);
 	if (substr($tel, 0, 2)=='00') {
 		$tel_prefix = '+'.substr($tel, 2, 2);
-		$tel = '+'.substr($tel, 2);
+		//$tel = '+'.substr($tel, 2);
+		$tel = '0'.substr($tel, 4);
 	}
 	elseif (substr($tel, 0, 1)=='0') {
 		$tel_prefix = '+33';
-		$tel = '+33'.substr($tel, 1);
+		//$tel = '+33'.substr($tel, 1);
 	}
 	else {
 		$tel_prefix = substr($tel, 0, 3);
+		$tel = '0'.substr($tel, 3);
 	}
 	if ($thirdparty->country_code=='FR' || empty($thirdparty->country_code)) {
 		$country_code = 'FR';
